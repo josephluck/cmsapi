0552b632e573a9c4f4542e46f4c98cd9e90148c4
Joseph Luck <joseph.luck@commusoft.co.uk>
2016-03-28 11:22:44 +0100

    Removing subdomain constraint

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ CmsApi::Application.routes.draw do
   devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' }, path: '/'  do
+                              path: '/'  do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
